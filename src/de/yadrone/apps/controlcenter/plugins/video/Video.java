45234f4389722e035c73bf21e7873985f534ecc7
zmoucl <zmoucl@WS-ZMOUCL.bbcnet-zh.local>
2017-05-11 10:47:57 +0200

    video

--- src/de/yadrone/apps/controlcenter/plugins/video/Video.java
+++ src/de/yadrone/apps/controlcenter/plugins/video/Video.java
@@ -4,7 +4,6 @@ import java.awt.image.BufferedImage;
 
 import de.yadrone.apps.controlcenter.ControlcenterController;
 import de.yadrone.base.IARDrone;
-import de.yadrone.base.command.VideoCodec;
 import de.yadrone.base.video.ImageListener;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
@@ -13,7 +12,7 @@ public class Video extends Thread implements ImageListener {
 
 	private IARDrone drone;
 	private ControlcenterController controller;
-	private boolean showWaiting = true;
+
 
 	public Video(final IARDrone drone, ControlcenterController controller) {
 		this.drone = drone;
@@ -22,14 +21,12 @@ public class Video extends Thread implements ImageListener {
 	}
 
 	public void run() {
-		drone.getCommandManager().setVideoCodec(VideoCodec.MP4_360P);
 		drone.getVideoManager().addImageListener(this);
 	}
 
 	public void imageUpdated(BufferedImage bImage) {
 		Image image = SwingFXUtils.toFXImage(bImage, null);
 		controller.draw(image);
-		System.out.println("Image");
 	}
 
 }
