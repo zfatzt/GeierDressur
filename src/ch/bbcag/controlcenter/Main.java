1c405714de6fc5643fdb4d1d3fd9a3c425d5ce2b
zfatzt <zfatzt@WS-ZFATZT.bbcnet-zh.local>
2017-05-19 10:00:53 +0200

    Myo error Dialog

--- src/ch/bbcag/controlcenter/Main.java
+++ src/ch/bbcag/controlcenter/Main.java
@@ -1,47 +1,59 @@
 package ch.bbcag.controlcenter;
 
-import com.thalmic.myo.Hub;
-import com.thalmic.myo.Myo;
-import com.thalmic.myo.enums.LockingPolicy;
-
 import ch.bbcag.controlcenter.plugins.altitude.Altitude;
 import ch.bbcag.controlcenter.plugins.battery.BatteryInDecimal;
 import ch.bbcag.controlcenter.plugins.connection.ConnectionState;
+import ch.bbcag.controlcenter.plugins.exceptions.ErrorDialogs;
 import ch.bbcag.controlcenter.plugins.keyboard.KeyboardCommandManager;
 import ch.bbcag.controlcenter.plugins.myo.BatteryListenerMyo;
 import ch.bbcag.controlcenter.plugins.speed.SpeedLabel;
 import ch.bbcag.controlcenter.plugins.video.Video;
+import com.thalmic.myo.Hub;
+import com.thalmic.myo.Myo;
+import com.thalmic.myo.enums.LockingPolicy;
 import de.yadrone.base.ARDrone;
 import de.yadrone.base.IARDrone;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
 	private IARDrone ardrone = null;
 	private String currentKey;
+    private ErrorDialogs errordialogs = new ErrorDialogs();
 
 	public static void main(String[] args) {
 		Application.launch(Main.class, args);
 	}
 
 	@Override
-	public void start(Stage stage) throws Exception {
+	public void start(Stage stage) {
 		stage.setOnCloseRequest((event -> System.exit(0)));
 		ardrone = new ARDrone();
 		ardrone.start();
 
 		Hub hub = new Hub("com.example.hello-myo");
 		Myo myo = hub.waitForMyo(10);
+		if (myo == null) {
+			errordialogs.errorDialogMyo();
+		}
 
 		hub.setLockingPolicy(LockingPolicy.LOCKING_POLICY_NONE);
 		KeyboardCommandManager cmdManager = new KeyboardCommandManager(ardrone);
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("View.fxml"));
 
 		// Key Listener
-		Scene scene = new Scene(loader.load());
+		Scene scene = null;
+		try {
+			scene = new Scene(loader.load());
+		} catch (IOException e) {
+            errordialogs.errorDialogScene();
+		}
 		scene.setOnKeyPressed((event) -> {
 			cmdManager.keyPressed(event);
 			setCurrentKey("KEY " + event.getCharacter());
@@ -50,6 +62,7 @@ public class Main extends Application {
 			cmdManager.keyReleased(event);
 			setCurrentKey("");
 		});
+
 
 		// Controller
 		Controlcenter c = loader.getController();
---
+++ src/ch/bbcag/controlcenter/plugins/exceptions/ErrorDialogs.java
@@ -0,0 +1,24 @@
+package ch.bbcag.controlcenter.plugins.exceptions;
+
+import javafx.scene.control.Alert;
+
+/**
+ * Created by zfatzt on 19.05.2017.
+ */
+public class ErrorDialogs {
+    private Alert alert = new Alert(Alert.AlertType.ERROR);
+    public void errorDialogMyo(){
+        alert.setTitle("Error Dialog");
+        alert.setHeaderText("No Myo Connected");
+        alert.showAndWait();
+    }
+
+    public void errorDialogScene(){
+        alert.setTitle("Error Dialog");
+        alert.setHeaderText("Failed to build Gui");
+        alert.showAndWait();
+
+
+    }
+
+}
